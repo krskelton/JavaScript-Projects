3e67ca8d9dac5610c30ea1c49a948abc8d09c37e
krskelton <kskelton82@gmail.com>
2020-12-22 13:24:44 -0700

    Add js demonstrating various methods and write functions

---
+++ Basic JavaScript Projects/Project1_expressions_alert/js/main.js
@@ -0,0 +1,27 @@
+window.alert("Hello, World!"); //using window.alert method
+
+document.write("Hello, World!"); //using document.write method
+
+var A = "This is the variable A."; //define string variable
+
+document.write(A); //print with document.write
+
+var backslash = "This can\'t be displayed without a backslash."; //demonstrating using the escape key
+
+document.write(backslash); //print with document.write
+
+var concateString = "This is some string" + " and I added more to it!"; //demostrating concatenation
+
+document.write(concateString); //print with document.write
+
+var mom = "Brenda", dad="Bill"; //demonstrating assigning multiple variables in one statement
+
+document.write(mom); //print with document.write
+
+var mom = mom.fontcolor("blue");//change color of text with .fontcolor()
+var dad = dad.fontcolor("purple");//change color of text with .fontcolor()
+
+document.write(mom + " " + dad); //concatenate and print with document.write
+
+var sum = 3+3; //creating an expression
+document.write(sum); //print with document.write
